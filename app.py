2b4a06443922b52b43ca3391c38bff08f9a6a2b5
Filip Marzecgit <filipm93@gmail.com>
2023-09-15 12:55:16 +0200

    replicate error fix

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def create_conversational_chain(vector_store):
         streaming = True,
         model = "replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781", 
         callbacks=[StreamingStdOutCallbackHandler()],
-        input = {"temperature": 0.01, "max_length" :500,"top_p":1},
+        model_kwargs = {"temperature": 0.01, "max_length" :500,"top_p":1},
         replicate_api_token="r8_co3XjbR2VQIXuMdRuK3EAnEzIGHeV8C2SBGMX",
         stop=getStoppingTokens())
     
